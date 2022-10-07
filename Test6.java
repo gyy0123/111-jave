d6f5fc768243368b584155a57a21b4809932261b
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-10-07 09:11:14 +0800

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Test6{
+    public static void main(String[] args){
+        int grade ;
+		Scanner input =new Scanner(System.in);
+		System.out.println("Enter first integer");
+		grade = input.nextInt();
+	  switch (grade) {
+	    case 90:
+	        System.out.println("A");
+	         break;
+		case 80:
+			 System.out.println("B");
+			 break;
+		case 70:
+			 System.out.println("C");
+			 break;
+		case 60:
+		     System.out.println("D");
+			 break;
+		default:	
+	         System.out.println("E");
+			 break;
+	  }
+
+}
+}
