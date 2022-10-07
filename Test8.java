c0d559fd24448fb407418224caa276a877ec853c
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-10-07 11:40:26 +0800

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test8{
+    public static void main(String[] args){
+        int grade= 0;
+		String space = "";
+		while(grade<10){
+			System.out.printf("%s",space);
+			System.out.printf("%d%n",grade) ;
+			grade=grade+1 ;
+			space=space+" ";
+			
+			
+		}
+        
+	}
+}
