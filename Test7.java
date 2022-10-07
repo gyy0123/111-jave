e82481b7a69dacfd582beca22c89b554bdf17b90
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-10-07 10:15:58 +0800

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Test7{
+    public static void main(String[] args){
+        int grade ;
+		Scanner input =new Scanner(System.in);
+		System.out.println("Enter first integer");
+		grade = input.nextInt();
+		if (grade>100){
+			System.out.println("error");
+		}  
+		if (grade<=100){
+		switch (grade/10) {
+	    
+		case 9:
+	    case 10:
+			 System.out.println("A");
+	         break;
+		case 8:
+			 System.out.println("B");
+			 break;
+		case 7:
+			 System.out.println("C");
+			 break;
+		case 6:
+		     System.out.println("D");
+			 break;
+		default:	
+	         System.out.println("E");
+			 break;
+	  }
+		}
+}
+}
