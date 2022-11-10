1f7bd4f545b0a8d78cff1101a5aa1f2b6b088618
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-11-10 23:17:29 +0800

    Add files via upload

--- Test14.java
+++ Test14.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
 
 
- 
 public class Test14{
    public static void main(String[] args) {
 
       for(int i = 1; i < 10; i++) {
 
           for(int j = 1; j < 10; j++) {
-			System.out.printf("%d*%d=%d\t,j,i,i*j")
+			System.out.printf("%d*%d=%2d ",i, j,  i * j);
 
           }
 
-         System.out.print("\n");}}}
+         System.out.print("");
+		 }
+					}
+}
