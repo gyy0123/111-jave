c4234160bfdf9167abfc05b79473235f9cb3f19b
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-11-25 09:59:18 +0800

    Add files via upload

---
+++ Test15.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test15{
+    public static void main(String[] args){
+		int width=0;
+    for (int i=-4;i<=4;i++){
+			
+			for (int k=-5;k<=5;k++){
+			
+			if(Math.abs(k)>width)	
+				System.out.print("*");
+			else 	
+				System.out.print(" ");	
+			}	
+			System.out.println("");
+			
+			if(i<0)
+			width++;
+			else
+			width--;
+		}		
+	} 
+	}
