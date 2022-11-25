0687cea12e9071a6fbdbda39e55ac8b821cccac3
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-11-25 11:40:11 +0800

    Add files via upload

---
+++ Test16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Test16{
+    public static void main(String[] args){
+	int x;
+	int y=7;
+    Scanner input=new Scanner(System.in);
+	System.out.println("Enter first");
+	x= input.nextInt();
+	if(x<=y)
+	System.out.println("85");
+	else
+	
+	System.out.printf("%d",85+(x-y)*5);
+	
+	
+	
+	
+	
+	
+	}	
+	}
