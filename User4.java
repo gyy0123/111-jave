1d0ba9ceb194094a8c9418ca37cd5860db951c49
gyy0123 <114204593+gyy0123@users.noreply.github.com>
2022-12-23 09:34:22 +0800

    Add files via upload

--- User4.java
+++ User4.java
@@ -1,40 +1,41 @@
 public class User4{
 public static void main (String[] args){
-int tmp,max=0,min=10,ave=0,mid=4,y,z=0;
-int [] myArr={7,5,4,9,8,1,2};
-int [] flag_arr =new int [myArr.length];	
-int flag_count=0;
-int flag_min=10;
+	int tmp,max=0,min=10,med=0,y;
+	float ave=0;
+	int [] myArr={7,5,4,9,8,1,2};
+	int [] flag =new int [myArr.length];
+	int flag_count=1;	
 	for (int x=0;x < myArr.length;x++ ){
 	
-	if (myArr[x]>max){
-		max =myArr[x];
-	}
-	if (myArr[x]<min){
-		min=myArr[x];	
-	}	
-	y=myArr[x];
-	ave=ave+y;
-	for(int j=0;j<myArr.length;j++){
-	if (flag_arr[j]==0&&myArr[j]<flag_min){
-		flag_min=myArr[j];
-		flag_arr[j]=flag_count;
-		flag_count++;
-	}
-		if(flag_count==4)
-			break;
+		if (myArr[x]>max){
+			max =myArr[x];
+		}
+		if (myArr[x]<min){
+			min=myArr[x];	
 		}	
-}
-	
-	for (int i=0;i<myArr.length;i++){
-	System.out.print(myArr[i]);
+		y=myArr[x];
+		ave=ave+y;
 	}
-	System.out.println("");
-	System.out.println(max);
-	System.out.println(min);
-	System.out.println(ave/6);
-	System.out.println(mid);
 	
-	
+	for (int i=0;i<4;i++){
+		int flag_min=1000;	
+		int flag_min_index=0;
+		for (int j=0;j<myArr.length;j++){	
+			if(myArr[j]<flag_min&&flag[j]==0){		
+				flag_min=myArr[j];
+				flag_min_index= j;
+			}
+		}
+		flag[flag_min_index]=flag_count;
+		flag_count++;
+		med = myArr[flag_min_index];
+	}
+				
+		System.out.println("");
+		System.out.println("max: " + max);
+		System.out.println("min: " + min);
+		System.out.println("ave: " + ave/7);
+		System.out.println("med: " + med);
 }
+	
 }
